1f222dd3fb4c695e39c6cefe2288dc85e04a56d3
Simon Colmer <simon_colmer@hotmail.com>
2013-04-08 22:26:19 +0100

    Namespace fixes

--- X-Volt/Properties/AssemblyInfo.cs
+++ X-Volt/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("X_Volt")]
+[assembly: AssemblyTitle("Xvolt")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("X_Volt")]
+[assembly: AssemblyProduct("Xvolt")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
