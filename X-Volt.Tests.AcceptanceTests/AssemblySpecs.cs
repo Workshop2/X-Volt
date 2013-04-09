fcfdd48ebab2ae2961aad4ccfbf701d9aa645e2c
Simon Colmer <simon_colmer@hotmail.com>
2013-04-09 09:43:26 +0100

    Got basic acceptance tests up and running

---
+++ X-Volt.Tests.AcceptanceTests/AssemblySpecs.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using SpecsFor.Mvc;
+using Xvolt;
+using Xvolt.App_Start;
+
+namespace X_Volt.Tests.AcceptanceTests
+{
+    [SetUpFixture]
+    public class AssemblySpecs
+    {
+        private SpecsForIntegrationHost _host;
+
+        [SetUp]
+        public void SetupTestRun()
+        {
+            var config = new SpecsForMvcConfig();
+            config.UseIISExpress()
+                .With(Project.Named("X-Volt"));
+                //.ApplyWebConfigTransformForConfig("Test");
+
+            //TODO: The order of registration matters right now, but it shouldn't. 
+            //config.RegisterArea<TasksAreaRegistration>();
+            config.BuildRoutesUsing(RouteConfig.RegisterRoutes);
+
+            //NOTE: You can use whatever browser you want.  For build servers, you can check an environment
+            //		variable to determine which browser to use, enabling you to re-run the same suite of
+            //		tests once for each browser. 
+            //config.UseBrowser(BrowserDriver.InternetExplorer);
+            config.UseBrowser(BrowserDriver.Chrome);
+            //config.UseBrowser(BrowserDriver.Firefox);
+
+            config.InterceptEmailMessagesOnPort(13565);
+            
+            _host = new SpecsForIntegrationHost(config);
+            _host.Start();
+        }
+
+        [TearDown]
+        public void TearDownTestRun()
+        {
+            _host.Shutdown();
+        }
+    }
+}
---
+++ X-Volt.Tests.AcceptanceTests/Routing/HomeTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using MvcContrib.TestHelper;
+using SpecsFor.Mvc;
+using SpecsFor;
+using Xvolt.Controllers;
+
+
+namespace X_Volt.Tests.AcceptanceTests.Routing
+{
+    public static class HomeTests
+    {
+        public class when_testing_routing_to_index : SpecsFor<MvcWebApp>
+        {
+            protected override void When()
+            {
+                SUT.NavigateTo<HomeController>(x => x.Index());
+            }
+
+            [Test]
+            public void then_route_should_map_to_index_page()
+            {
+                SUT.Route.ShouldMapTo<HomeController>(x => x.Index());
+            }
+        }
+    }
+}
