56712938a9d7dce8988cf33da9b550c98295761e
Simon Colmer <simon_colmer@hotmail.com>
2013-04-10 10:20:58 +0100

    Some work at restarting. Acceptance not working

---
+++ Xvolt.Web/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Xvolt.Web.Models;
+
+namespace Xvolt.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            var tempData = new List<News>
+            {
+                new News{ Title = "Test 1" },
+                new News{ Title = "Test 2" },
+            };
+
+            return View(tempData);
+        }
+
+    }
+}
