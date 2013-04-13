ff06d4723d3af17328c9fea399a9217f0f153297
Simon Colmer <simon_colmer@hotmail.com>
2013-04-13 21:40:23 +0100

    Got some css stuff working

---
+++ Xvolt.Web/App_Start/BundlesConfig.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Optimization;
+
+namespace Xvolt.Web
+{
+    public class BundlesConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            SetupJquery(bundles);
+        }
+
+        private static void SetupJquery(BundleCollection bundles)
+        {
+            // Javascript
+            var jqueryScript = new[] 
+            { 
+                "~/Scripts/jquery-{version}.js", 
+                "~/Scripts/jquery.metrojs.js" 
+            };
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(jqueryScript));
+
+            // CSS/Styles
+            var jqueryStyles = new[] 
+            { 
+                "~/Styles/jquery.metrojs.css" 
+            };
+
+            bundles.Add(new StyleBundle("~/bundles/jquery-style").Include(jqueryStyles));
+        }
+    }
+}
