4d5e75895e5b64a5da46ae21824c4ad31fe4b50c
Simon Colmer <simon_colmer@hotmail.com>
2013-04-17 09:49:05 +0100

    Um, conflict, not sure..err

---
+++ Tests/Xvolt.Tests.AcceptanceTests/Routing/Api/NewsArticleTests.cs
@@ -0,0 +1,16 @@
+﻿namespace Xvolt.Tests.AcceptanceTests.Routing.Api
+{
+    public class NewsArticleTests
+    {
+        protected override void When()
+        {
+            SUT.NavigateTo<HomeController>(x => x.Index());
+        }
+
+        [Test]
+        public void then_route_should_map_to_index_page()
+        {
+            SUT.Route.ShouldMapTo<HomeController>(x => x.Index());
+        }
+    }
+}
