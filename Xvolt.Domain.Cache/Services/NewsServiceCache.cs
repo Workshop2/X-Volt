4d32b9afd1ac777263a6629b06e52eddcfeb21a7
Simon Colmer <simon_colmer@hotmail.com>
2014-03-29 22:55:46 +0100

    Adding in missing files

---
+++ Xvolt.Domain.Cache/Services/NewsServiceCache.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Xvolt.Domain.Entities;
+using Xvolt.Domain.Services.Interfaces; 
+
+namespace Xvolt.Domain.Cache.Services
+{
+    public class NewsServiceCache : INewsService
+    {
+        private readonly INewsService _child;
+
+        public NewsServiceCache(INewsService child)
+        {
+            _child = child;
+        }
+
+        private IEnumerable<NewsArticle> _newsArticleCache; 
+        public IEnumerable<NewsArticle> GetNewsArticles()
+        {
+            return _newsArticleCache ?? (_newsArticleCache = _child.GetNewsArticles());
+        }
+
+        public void Dispose()
+        {
+            if (_child != null)
+            {
+                _child.Dispose();
+            }
+        }
+    }
+}
